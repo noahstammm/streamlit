db0180010b951481bcced8f369e879f5f7c6d01e
noahstammm <stammnoa@students.zhaw.ch>
2023-04-26 18:35:28 +0200

    new test

--- request_service.py
+++ request_service.py
@@ -25,7 +25,7 @@ def generate_response(message, role, response_length):
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     response_json = response.json()
-    print(response_json)
+    print("My RESPONSE______________________________________________________________"+response + response_json)
     return response_json['choices'][0]['message']['content'].strip()
 
 
